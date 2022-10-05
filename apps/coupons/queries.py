bd95ba202c97efc1ab2cf25bd82022feebed39bb
BigPixelLab1 <iam-namber-1@mail.ru>
2022-10-05 21:17:47 +0300

    Исправлены запросы под новый формат

--- apps/coupons/queries.py
+++ apps/coupons/queries.py
@@ -3,20 +3,28 @@ from . import models
 
 
 def get_coupon_by_code(code: str) -> models.Coupon:
-    return database.single(models.Coupon, """
-        select * from "Coupon"
-        where
-            code = %(code)s
-            -- no is_expired check cause we just need coupon, expired or not
-        limit 1
-    """, code=code)
+    return database.single(
+        models.Coupon,
+        """
+            select * from "Coupon"
+            where
+                code = %(coupon_code)s
+                -- no is_expired check cause we just need coupon, expired or not
+            limit 1
+        """,
+        coupon_code=code
+    )
 
 
-def is_promo_used_by_customer(code: str, customer: int):
-    return database.single_value("""
-        -- If there is at least one order from customer with given coupon, coupon considered as used
-        select count(*) from "Order" O
-        where
-            O.coupon = %s and
-            O.customer_id = %s
-    """, code, customer) > 0
+def is_promo_used_by_customer(code: str, customer: int) -> bool:
+    return database.single_value(
+        """
+            -- If there is at least one order from customer with given coupon, coupon considered as used
+            select count(*) > 0 from "Order" O
+            where
+                O.coupon = %(coupon_code)s and
+                O.customer_id = %(customer_id)s
+        """,
+        coupon_code=code,
+        customer_id=customer
+    )
