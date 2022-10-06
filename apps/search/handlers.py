9fad83baae6ad055ad4bdd64ae5ec26d9e33777a
BigPixelLab1 <iam-namber-1@mail.ru>
2022-10-06 07:46:28 +0300

    Небольшие исправления

--- apps/search/handlers.py
+++ apps/search/handlers.py
@@ -1,20 +1,22 @@
+import logging
 from pathlib import Path
 
 from aiogram.types import Message
 
-import gls
 from utils import template
 from . import queries
 from . import utils
 
 TEMPLATES = Path('apps/search/templates')
+logger = logging.getLogger(__name__)
 
 
 async def search_handler(message: Message):
     service, score = utils.get_suggested_service(
-        utils.transliterate(message.text),
+        search := utils.transliterate(message.text),
         queries.get_services()
     )
+    logger.info(f'Searching: {search}')
     # Перед получением списка подписок необходимо снять резервацию
     # с ключей для которых она истекла, иначе, если для заказа
     # подписки требуется свободный ключ, его может не оказаться
@@ -24,5 +26,5 @@ async def search_handler(message: Message):
     await template.render(TEMPLATES / 'suggestions.xml', {
         'sub_plans': sub_plans,
         'service': service,
-        'show': score != 0
+        'show': score > 0.01
     }).send(message.chat.id)
