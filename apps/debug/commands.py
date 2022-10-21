a429d5855fd0a0bd71fcc4528f4c305d81d64518
BigPixelLab1 <iam-namber-1@mail.ru>
2022-10-21 10:32:45 +0300

    Убран мусор для теста

--- apps/debug/commands.py
+++ apps/debug/commands.py
@@ -1,17 +1,8 @@
-from dataclasses import dataclass
-
 from aiogram import Router
-from aiogram.types import CallbackQuery
 
 from . import handlers
-from ..purchase.callbacks import CheckBillCallback
-from ..purchase.handlers import bill_paid_handler
 
 router = Router()
 
 router.message(commands=['debug'])(handlers.command_debug)
 router.callback_query()(handlers.missed_query_handler)
-
-router.message(
-	commands=['test']
-)(handlers.test_handler)
--- apps/purchase/commands.py
+++ apps/purchase/commands.py
@@ -1,7 +1,4 @@
-from dataclasses import dataclass
-
 from aiogram import Router
-from aiogram.types import CallbackQuery
 
 from . import callbacks, handlers
 
@@ -11,27 +8,9 @@ router.callback_query(
     callbacks.BuySubscriptionCallback.filter()
 )(handlers.buy_handler)
 
-
-def check_substitution(query: CallbackQuery):
-    @dataclass
-    class Amount:
-        value: int
-
-    @dataclass
-    class Bill:
-        amount: Amount
-
-    return {'bill': Bill(Amount(500))}
-
-
-# router.callback_query(
-#     callbacks.CheckBillCallback.filter()
-# )(handlers.check_bill_handler)
-
 router.callback_query(
-    callbacks.CheckBillCallback.filter(),
-    check_substitution
-)(handlers.bill_paid_handler)
+    callbacks.CheckBillCallback.filter()
+)(handlers.check_bill_handler)
 
 router.callback_query(
     callbacks.PosInQueueCallback.filter()
