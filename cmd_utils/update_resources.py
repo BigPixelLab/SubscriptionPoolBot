fa3badb0ce1578034aa0665243cf08492a8efe6c
BigPixelLab1 <iam-namber-1@mail.ru>
2022-10-23 23:11:29 +0300

    Больше нет необходимости в использовании стороннего хостинга

--- cmd_utils/update_resources.py
+++ cmd_utils/update_resources.py
@@ -12,6 +12,8 @@ from utils import database
 
 logger = logging.getLogger(__name__)
 
+PHOTO, DOCUMENT = range(2)
+
 
 # Токен бота, который будет использован для загрузки
 # (не имеет особого значения, главное чтобы был действительным)
@@ -22,32 +24,53 @@ BOT_TOKEN = settings.BOT_TOKEN
 # нужно отправить куда-то файл, чтобы получить его id)
 SPAM_CHAT = 1099569178
 
-# Ресурсы для загрузки, в формате: (путь, имя, запрос)
+# Ресурсы для загрузки, в формате: (тип, путь, имя, запрос)
+# Тип - PHOTO или DOCUMENT
 # Путь - путь к файлу относительно директории проекта.
 # Имя - имя, которое будет передано telegram-у (таким именем будет обладать файл на сервере).
 # Запрос - запрос к базе данных, который будет выполнен после успешной загрузки файла,
 #       получает file_id в качестве аргумента
 RESOURCES = [
-    ('resources/NbuyVideo.mp4', 'NETFLIX.mp4',
-     """ update "Service" set bought = %(file_id)s where name = 'netflix' """),
-    ('resources/SbuyVideo.mp4', 'SPOTIFY.mp4',
-     """ update "Service" set bought = %(file_id)s where name = 'spotify' """)
+    # (DOCUMENT, 'resources/NETFLIX-BOUGHT.mp4', 'NETFLIX-BOUGHT.mp4',
+    #  """ update "Service" set bought = %(file_id)s where name = 'netflix' """),
+
+    # (DOCUMENT, 'resources/SPOTIFY-BOUGHT.mp4', 'SPOTIFY-BOUGHT.mp4',
+    #  """ update "Service" set bought = %(file_id)s where name = 'spotify' """),
+
+    # (PHOTO, 'resources/NETFLIX-BANNER.jpg', 'NETFLIX-BANNER.jpg',
+    #  """ update "Service" set banner = %(file_id)s where name = 'netflix' """),
+    #
+    # (PHOTO, 'resources/SPOTIFY-BANNER.jpg', 'SPOTIFY-BANNER.jpg',
+    #  """ update "Service" set banner = %(file_id)s where name = 'spotify' """),
+    #
+    # (PHOTO, 'resources/INTRO.jpg', 'INTRO.jpg', None),
 ]
 
 
 async def on_startup():
-    for i, (path, name, query) in enumerate(RESOURCES):
+    methods = {
+        DOCUMENT: (gls.bot.send_document, lambda m: m.document.file_id),
+        PHOTO: (gls.bot.send_photo, lambda m: m.photo[0].file_id)
+    }
+
+    for i, (_type, path, name, query) in enumerate(RESOURCES):
         await gls.bot.send_message(SPAM_CHAT, f'UPLOADING "{name}" ({i + 1}/{len(RESOURCES)})...')
 
+        send_stuff, extract_file_id = methods[_type]
+
         try:
-            message = await gls.bot.send_document(SPAM_CHAT, FSInputFile(path, name))
+            message = await send_stuff(SPAM_CHAT, FSInputFile(path, name))
         except TelegramBadRequest:
             await gls.bot.send_message(SPAM_CHAT, 'Failed to upload resource')
             continue
 
-        file_id = message.document.file_id
-        database.execute(query, query % {'file_id': file_id}, file_id=file_id)
+        file_id = extract_file_id(message)
+
+        if query:
+            database.execute(query, query % {'file_id': file_id}, file_id=file_id)
+
         await gls.bot.send_message(SPAM_CHAT, f'<code>{file_id}</code>')
+
     await gls.bot.send_message(SPAM_CHAT, 'Done.')
 
 
