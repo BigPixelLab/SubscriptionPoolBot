6c044b5937dd9f62a68d33480ad9514975766ee6
BigPixelLab1 <iam-namber-1@mail.ru>
2023-05-15 12:05:05 +0300

    Тест шаблонизатора

--- apps/debug/handlers.py
+++ apps/debug/handlers.py
@@ -2,8 +2,13 @@
 import aiogram.types
 
 import response_system as rs
+import response_system_extensions as rse
 
 
 async def unhandled_callback_query(query: aiogram.types.CallbackQuery):
     """ ... """
     return rs.send(f'Unhandled query: {query.data}')
+
+
+async def test_handler(_):
+    return rse.tmpl_send('apps/debug/templates/test.xml', {'a': 5})
