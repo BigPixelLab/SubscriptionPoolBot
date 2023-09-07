be17989ff4491e6649ec82d74fc25e1fe642f72f
Romver <roma_zverev2002@mail.ru>
2023-09-07 14:10:47 +0300

    Добавить новый app - statistics

---
+++ apps/statistics/models.py
@@ -0,0 +1,71 @@
+import datetime
+import json
+import peewee
+import gls
+
+from typing import Optional, Any
+from apps.botpiska.models import Client
+
+
+class JSONField(peewee.TextField):
+    def db_value(self, value: Optional[Any]):
+        if value is None:
+            return None
+        return json.dumps(value)
+
+    def python_value(self, value: Optional[str]) -> Any:
+        if value is not None:
+            return json.loads(value)
+
+
+class StatisticTypeAction(gls.BaseModel):
+    """ ... """
+
+    """ ID типа действия """
+    id = peewee.CharField(primary_key=True)
+    """ Название действия """
+    title = peewee.CharField()
+
+    class Meta:
+        table_name = 'StatisticTypeAction'
+
+
+class Statistic(gls.BaseModel):
+    """ ... """
+
+    client = peewee.ForeignKeyField(Client, on_delete='NO ACTION')
+    """ Тип действия """
+    action = peewee.ForeignKeyField(StatisticTypeAction, backref='statistics', on_delete='NO ACTION')
+    """ Данные действия """
+    # data = peewee.CharField(null=True)
+    data = JSONField()
+    """ Дата создания """
+    created_at = peewee.DateTimeField()
+
+
+    class Meta:
+        table_name = 'Statistic'
+        primary_key = False
+
+
+    @classmethod
+    def record(cls, action: str, user_id: int = 0, /, **data):
+        """ Запись активности пользователя
+        :param user_id: ID пользователя.
+        :param action: тип действия
+        :param data: данные, которые были получены при действии пользователя
+        :param now: дата создания
+        """
+
+        # if not cls.is_user_action(user_id, action):
+        try:
+            new_object = Statistic.create(
+                client=user_id,
+                action=action,
+                data=data,
+                created_at=datetime.datetime.now()
+            )
+            return new_object
+        except peewee.IntegrityError:
+            pass
+
